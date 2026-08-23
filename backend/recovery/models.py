704d9e4a6beeefa62fc49ecda65347ec5952dead
Abhijeet Waghmare <abhijeetw035@gmail.com>
2026-08-23 14:40:47 +0530

    feat: create Transaction model

--- backend/recovery/models.py
+++ backend/recovery/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Transaction(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=50)
+    failure_reason = models.CharField(max_length=100)
+    customer_id = models.CharField(max_length=100)
+    segment = models.CharField(max_length=100)
+    risk_level = models.CharField(max_length=20)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Transaction {self.id} - {self.amount} - {self.failure_reason}"
